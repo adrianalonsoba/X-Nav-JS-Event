933ab1ab510fb7649721be33f3a97675ad923cc2
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-02-09 18:12:31 +0100

    añadido funcion con nombre

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function changer (id, newValue) {
 
 var input_box= document.getElementById('text');
 
-input_box.addEventListener('input',function(e){
+
+handler=function(e){
 	changer('text_copy',input_box.value);
-})
+}
+
+input_box.addEventListener('input',handler);
+
